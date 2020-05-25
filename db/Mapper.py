c96b752ca1a340f9f84b096914a694f81fde2e34
KieschkelDo <dk108@hdm-stuttgart.de>
2020-05-25 16:57:02 +0200

    Mapper-Klasse hinzugefügt. Methoden bisher unvollständig. Connection zur lokalen DB enthalten.

---
+++ db/Mapper.py
@@ -0,0 +1,45 @@
+import mysql.connector
+from abc import ABC, abstractmethod
+from contextlib import AbstractContextManager
+
+class Mapper(AbstractContextManager, ABC):
+
+    def __init__(self):
+        self._connector = None
+
+    def __enter__(self):
+        """
+        Hier exestiert bisher nur der connect zur lokalen DB.
+        """
+
+        self._connector = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="root",
+            database="shoppingtest"
+            )
+
+        print(type(self._connector))
+    def __exit__(self, type, value, traceback):
+
+        self._connector.close()
+
+    @abstractmethod
+    def insert(self, object):
+        pass
+
+    """
+    Weitere Methoden folgen. Teste momentan noch mit dem Insert in den jeweiligen Mapper-Klassen herum.
+    """
+
+
+#mycursor.execute("CREATE TABLE Person (name VARCHAR(50), email NVARCHAR(255), creation_date TIME, personID int PRIMARY KEY AUTO_INCREMENT)")
+
+#sql1 = "INSERT INTO person (name, email) VALUES (%s, %s)"
+#data1 = ("Dominik", "dk108@hdm-stuttgart.de")
+
+#mycursor.execute(sql1, data1)
+#db.commit()
+
+#mycursor.execute("DESCRIBE Person")
+
