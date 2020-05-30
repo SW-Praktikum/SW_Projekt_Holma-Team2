063611c27666d934fa240621fb1ab67a29f00e4d
KieschkelDo <dk108@hdm-stuttgart.de>
2020-05-30 10:42:47 +0200

    find_by_id und find_by_name getestet und ergänzt.

--- src/db/UserMapper.py
+++ src/db/UserMapper.py
@@ -28,9 +28,61 @@ class UserMapper(Mapper):
 
         return result
 
+    def find_by_id(self, id):
+
+        result = None
+        cursor = self._connection.cursor()
+        command = "SELECT id, name, email, google_user_id FROM user WHERE id={}".format(id)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        try:
+            (id, name, email, google_id) = tuples[0]
+            user = User()
+            user.set_id(id)
+            user.set_name(name)
+            user.set_email(email)
+            user.set_google_id(google_id)
+            result = user
+        except IndexError:
+            result = None
+
+        self._connection.commit()
+        cursor.close()
+
+        return result
+
+
+
+    def find_by_name(self, name):
+
+        result = []
+        cursor = self._connection.cursor()
+        command = "SELECT id, name, email, google_user_id FROM user WHERE name LIKE '{}' ORDER BY name".format(name)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        for (id, name, email, google_id) in tuples:
+            user = User()
+            user.set_id(id)
+            user.set_name(name)
+            user.set_email(email)
+            user.set_google_id(google_id)
+            result.append(user)
+
+        self._connection.commit()
+        cursor.close()
+
+        return result
 
 if (__name__ == "__main__"):
     with UserMapper() as mapper:
         result = mapper.find_all()
         for user in result:
             print(user)
+"""
+if (__name__ == "__main__"):
+    with UserMapper() as mapper:
+        result = mapper.find_by_id(1)
+        print(user)
+"""
