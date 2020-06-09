a2a40d240fea8126aff08840eba188834ab74cc7
Jonas Götz <jg133@hdm-stuttgart.de>
2020-06-09 13:53:45 +0200

    added styleguide

---
+++ my-app/styleguide/precache-manifest.17fce0d3ce6ebb4b31d184c218be7b49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6742680f12877e3d3e4d",
+    "url": "build/2.6742680f.js"
+  },
+  {
+    "revision": "1f3f2ede4ff65e9672b306fa432e3a4a",
+    "url": "build/2.6742680f.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1e58a59d22b66b0cfe9",
+    "url": "build/bundle.b1e58a59.js"
+  },
+  {
+    "revision": "2187c5a4ad3eb8b490c5",
+    "url": "build/main.2187c5a4.js"
+  },
+  {
+    "revision": "73d08586c122d4b34200989df99096c7",
+    "url": "index.html"
+  }
+]);
