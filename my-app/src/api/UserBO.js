6f227813de46a74d36fa3079750cbc96a10594ec
yassi <yassine_bt@hotmail.de>
2020-06-09 18:35:51 +0200

    Kommentar gelöscht

--- my-app/src/api/UserBO.js
+++ my-app/src/api/UserBO.js
@@ -34,7 +34,6 @@ export default class UserBO extends BusinessObject {
                 result.push(u)
             })
         } else {
-
             let u = users;
             Object.setPrototypeOf(u, UserBO.prototype)
             result.push(u)
