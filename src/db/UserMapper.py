72b52900d6f1931584dd0ffd5f6f028c7d296c39
KieschkelDo <dk108@hdm-stuttgart.de>
2020-06-19 19:18:57 +0200

    UserMapper test.

--- src/db/UserMapper.py
+++ src/db/UserMapper.py
@@ -80,14 +80,15 @@ class UserMapper(Mapper):
 
     def insert(self, user):
         cursor = self._connection.cursor()
-        command = "INSERT INTO user (user_id, name, creation_date, email, google_id, last_updated) VALUES ({},{},{},{},{},{})".format(
-            user.get_id(),
-            user.get_name(),
-            user.get_creation_date(),
-            user.get_email(),
-            user.get_google_id(),
-            user.get_last_updated())
-        cursor.execute(command)
+        command = "INSERT INTO user (user_id, name, creation_date, email, google_id, last_updated) VALUES (%s,%s,%s,%s,%s,%s)".
+        data = (user.get_id(),
+                user.get_name(),
+                user.get_creation_date(),
+                user.get_email(),
+                user.get_google_id(),
+                user.get_last_updated())
+
+        cursor.execute(command, data)
 
         self._connection.commit()
         cursor.close()
