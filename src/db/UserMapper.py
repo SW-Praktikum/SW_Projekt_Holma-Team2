9faddc053d46ad93c688b0f625aed174a9a359f2
iamnotturner <iamnotturner@turnermail.de>
2020-06-19 18:59:08 +0200

    Bugfix UserMapper - data anstatt format

--- src/db/UserMapper.py
+++ src/db/UserMapper.py
@@ -91,8 +91,13 @@ class UserMapper(Mapper):
         print("-----------------")
 
         command = "INSERT INTO user (user_id, name, creation_date, email, google_id, last_updated) VALUES (%s, %s, %s, %s, %s, %s)"
-        data = (user.get_id(), user.get_name(), user.get_creation_date(), user.get_email(), user.get_google_id(),
+        data = (user.get_id(),
+                user.get_name(),
+                user.get_creation_date(),
+                user.get_email(),
+                user.get_google_id(),
                 user.get_last_updated())
+
         cursor.execute(command, data)
 
         self._connection.commit()
@@ -103,11 +108,13 @@ class UserMapper(Mapper):
     def update(self, user):
 
         cursor = self._connection.cursor()
-        command = "UPDATE user SET name={}, email={}, last_updated={}  WHERE user_id={}".format(user.get_name(),
-                                                                                                user.get_email(),
-                                                                                                user.get_last_updated(),
-                                                                                                user.get_id())
-        cursor.execute(command)
+        command = "UPDATE user SET name=%s, email=%s, last_updated=%s  WHERE user_id=%s"
+        data = (user.get_name(),
+                user.get_email(),
+                user.get_last_updated(),
+                user.get_id())
+
+        cursor.execute(command, data)
 
         self._connection.commit()
         cursor.close()
@@ -118,8 +125,9 @@ class UserMapper(Mapper):
 
         cursor = self._connection.cursor()
 
-        command = "DELETE FROM user WHERE user_id={}".format(user.get_id())
-        cursor.execute(command)
+        command = "DELETE FROM user WHERE user_id=%s"
+        data = (user.get_id())
+        cursor.execute(command, data)
 
         self._connection.commit()
         cursor.close()
