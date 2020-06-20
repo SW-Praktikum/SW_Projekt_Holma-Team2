e0a61db165f4b556e0f17bf5141bcb525594f9c9
unknown <ck169@hdm-stuttgart.de>
2020-06-20 10:15:34 +0200

    Added Navigation, Theme and edit App.js

---
+++ holma-app/src/components/Layout/Navigation.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import {Paper, Typography, Tabs, Tab, AppBar }from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import Group from '@material-ui/icons/Group';
+import List from '@material-ui/icons/List';
+import GroupAdd from '@material-ui/icons/GroupAdd';
+import Assessment from '@material-ui/icons/Assessment';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import Settings from '@material-ui/icons/Settings';
+import theme from '../Theme';
+
+
+const useStyles = makeStyles({
+  root: {
+    flexGrow: 1,
+    maxWidth: 1000,
+  },
+});
+
+export default function IconLabelTabs() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+
+  return (
+    <div>
+      <div>
+        <Typography variant='h3' component='h1' align='center'>
+          Header
+        </Typography>
+        <Typography variant='h4' component='h1' align='center'>
+          Navigation
+        </Typography>
+        
+        </div>
+    <Paper square className={classes.root}>
+      <div>
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant="fullWidth"
+        indicatorColor="secondary"
+        textColor="black"
+        aria-label="icon label tabs example"
+      >
+        <Tab icon={<Group />} label="Gruppen" value="1"/>
+        <Tab icon={<List />} label="Listen" value="2"/>        
+      </Tabs>
+
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant="fullWidth"
+        indicatorColor="secondary"
+        textColor="black"
+        aria-label="icon label tabs example"
+      >
+        <Tab icon={<GroupAdd />} label="Gruppe hinzufügen" value="3" />
+        <Tab icon={<Assessment />} label="Statistik" value="4" />
+        
+      </Tabs>
+
+      <Tabs
+        value={value}
+        onChange={handleChange}
+        variant="fullWidth"
+        indicatorColor="secondary"
+        textColor="red"
+        aria-label="icon label tabs example"
+      >
+        <Tab icon={<AccountCircle />} label="Mein Konto" value="5" />
+        <Tab icon={<Settings />} label="Settings" value="6" />
+        
+      </Tabs>
+      </div>
+    </Paper>
+    </div>
+  );
+}
