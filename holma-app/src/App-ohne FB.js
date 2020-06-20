09ab7f561d2f499a3b2ad9daf3c0f2184d97892b
Dennis-La <59310118+Dennis-La@users.noreply.github.com>
2020-06-20 16:29:25 +0200

    Firebase in App.js übernommen

---
+++ holma-app/src/App-ohne FB.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css';
+
+import GroupAddDialog from './components/dialogs/GroupAddDialog';
+import ListWithBoxes from "./components/GroupListEntry"
+function App() {
+  //ListWithBoxes
+  return (
+    <div className="App">
+      <header className="App-header">
+        <ListWithBoxes /> 
+      </header>
+    </div>
+  );
+}
+ 
+export default App;
