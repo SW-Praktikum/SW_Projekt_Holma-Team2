8669f66d37c931e0bac876d7afb2ad31d369f35a
Dennis-La <59310118+Dennis-La@users.noreply.github.com>
2020-06-20 13:54:18 +0200

    Anpassung GroupListEntry

--- holma-app/src/components/GroupListEntry.js
+++ holma-app/src/components/GroupListEntry.js
@@ -4,15 +4,11 @@ import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
-import Collapse from '@material-ui/core/Collapse';
-import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 import { red } from '@material-ui/core/colors';
-import AccountCircle from '@material-ui/icons/AccountCircle';
-import {Paper, Typography, Tabs, Tab, AppBar, CardActionArea }from '@material-ui/core';
+import {Typography, CardActionArea }from '@material-ui/core';
 
 
 class GroupEntry extends Component {
@@ -41,8 +37,8 @@ const useStyles = makeStyles({
     avatar: {
         backgroundColor: red,
       },
-      media: {
-        height: 0,
+    media: {
+        height: 10,
         paddingTop: '56.25%',
         marginTop:'30'
       },
