e4b6fa2a35dea841d3ea4a49e60c29c24723aec6
iamnotturner <iamnotturner@turnermail.de>
2020-06-20 10:09:11 +0200

    PEP8 Zeilenlänge angepasst

--- src/bo/Group.py
+++ src/bo/Group.py
@@ -5,17 +5,20 @@ from bo.BusinessObject import BusinessObject
 
 class Group(BusinessObject):
     def __init__(self):
-        """Gruppe, der Nutzer und Einkaufslisten hinzugefügt, oder entfernt werden. Eine Gruppe
-        verfügt über Standardartikel, die bei der Erstellung von Einkaufslisten (wahlweise)
-        hinzugefügt werden."""
+        """Gruppe, der Nutzer und Einkaufslisten hinzugefügt, oder
+        entfernt werden. Eine Gruppe verfügt über Standardartikel, die
+        bei der Erstellung von Einkaufslisten (wahlweise) hinzugefügt
+        werden."""
+
         super().__init__()
         self._owner = None  # nur als id (Fremdschlüssel)
 
     def __str__(self):
-        return "Group: {} {}, created: {}, owned by: {}, last changed: {}".format(self.get_id(), self.get_name(),
-                                                                                  self.get_creation_date(),
-                                                                                  self.get_owner(),
-                                                                                  self.get_last_updated())
+        return "Group: {} {}, created: {}, owned by: {}, " \
+               "last changed: {}".format(self.get_id(), self.get_name(),
+                                         self.get_creation_date(),
+                                         self.get_owner(),
+                                         self.get_last_updated())
 
     def get_owner(self):
         return self._owner
@@ -28,8 +31,10 @@ class Group(BusinessObject):
         result = {
             "id": obj.get_id(),
             "name": obj.get_name(),
-            "creationDate": datetime.strftime(obj.get_creation_date(), "%Y-%m-%dT%H:%M:%S.%fZ"),
-            "lastUpdated": datetime.strftime(obj.get_last_updated(), "%Y-%m-%dT%H:%M:%S.%fZ"),
+            "creationDate": datetime.strftime(obj.get_creation_date(),
+                                              "%Y-%m-%dT%H:%M:%S.%fZ"),
+            "lastUpdated": datetime.strftime(obj.get_last_updated(),
+                                             "%Y-%m-%dT%H:%M:%S.%fZ"),
             "owner": obj.get_owner()
         }
         return result
@@ -39,8 +44,10 @@ class Group(BusinessObject):
         group = Group()
         group.set_id(dictionary["id"])
         group.set_name(dictionary["name"])
-        group.set_creation_date(datetime.strptime(dictionary["creationDate"], "%Y-%m-%dT%H:%M:%S.%fZ"))
-        group.set_creation_date(datetime.strptime(dictionary["lastUpdated"], "%Y-%m-%dT%H:%M:%S.%fZ"))
+        group.set_creation_date(datetime.strptime(dictionary["creationDate"],
+                                                  "%Y-%m-%dT%H:%M:%S.%fZ"))
+        group.set_creation_date(datetime.strptime(dictionary["lastUpdated"],
+                                                  "%Y-%m-%dT%H:%M:%S.%fZ"))
         group.set_owner(dictionary["owner"])
         return group
 
