e4a63dbca629984d2b57be9ba7ac39d966c5eeb0
unknown <ck169@hdm-stuttgart.de>
2020-06-21 12:51:23 +0200

    Themes edit

--- holma-app/src/components/Theme.js
+++ holma-app/src/components/Theme.js
@@ -11,7 +11,7 @@ const theme = createMuiTheme({
       primary: {
         contrastText: white,
         dark: colors.indigo[900],
-        main: colors.indigo[500],
+        main: colors.red[800],
         light: colors.indigo[100]
       },
       secondary: {
