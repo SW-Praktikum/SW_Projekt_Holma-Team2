e24a0cab538d2dd197552520149ef81033abe24d
Dennis-La <59310118+Dennis-La@users.noreply.github.com>
2020-06-23 18:25:00 +0200

    Test

---
+++ holma-app/src/api/BusinessObject.js
@@ -0,0 +1,51 @@
+export default class BusinessObject {
+
+    constructor(name) {
+        this.id = 0;
+        this.name = name;
+        this.creationDate = new Date();
+        this.lastUpdated = this.creationDate;
+    }
+
+
+    getId() {
+        return this.id
+    }
+
+    getName() {
+        return this.name
+    }
+
+    getCreationDate() {
+        return this.creationDate
+    }
+
+    getLastUpated() {
+        return this.lastUpated
+    }
+
+    setId(id) {
+        this.id = id
+    }
+
+    setName(name) {
+        this.name = name
+    }
+
+    setCreationDate(creationDate) {
+        this.creationDate = creationDate
+    }
+
+    setLastUpated(lastUpated) {
+        this.lastUpated = lastUpated
+    }
+
+    toString() {
+        let result = '';
+        for (var prop in this) {
+            result += prop + ': ' + this[prop] + ' ';
+        }
+        return result;
+    }
+
+}
