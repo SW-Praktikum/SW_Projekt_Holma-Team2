ecdaf824702404b1a3ad85be038b8f7c4216e882
Dennis-La <59310118+Dennis-La@users.noreply.github.com>
2020-06-24 16:04:21 +0200

    GroupEdit und GroupEditDialog Components erstellt

--- holma-app/src/App.js
+++ holma-app/src/App.js
@@ -15,6 +15,8 @@ import GroupEntries from './components/GroupEntries';
 import AppAPI from './api/AppAPI'
 import UserBO from './api/UserBO';
 import GroupList from './components/GroupList';
+import GroupInformation from './components/GroupEdit';
+import Groupmember from './components/GroupEditDialog';
 
 
 class App extends React.Component {
@@ -130,8 +132,7 @@ class App extends React.Component {
                   <Redirect to='/group' />
                   <Route exact path='/group'>
                   <Navigation />
-                    <GroupEntries user={user}/>
-                    <GroupAddDialog user={user}/> 
+                    <GroupList user={user}/>
             <div style={{fontStyle: "italic"}}><br/><b>User:</b> {user.getName()} | <b>ID:</b> {user.getId()} | <b>Google ID:</b> {user.getGoogleId()} | <b>Member since:</b> {user.getCreationDate()}</div>
 
                   </Route>
---
+++ holma-app/src/components/GroupEdit.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import AppAPI from '../api/AppAPI';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import CardActionArea from '@material-ui/core/CardActionArea';
+//import Groupmember from './GroupEditDialog';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Fab from '@material-ui/core/Fab';
+import EditIcon from '@material-ui/icons/Edit';
+
+
+class GroupInformation extends Component {
+  render(){
+    return (
+      <div>
+      <Box m={5} />
+        <Typography  variant="h4" gutterBottom>
+          Groupdetails
+        </Typography>
+      <Box m={5} />
+      <Grid container spaching={3}>
+        <Grid item xs={8} sm={2}>
+          <Typography variant="h6" gutterBottom>Groupname</Typography>
+        </Grid>
+        <Grid item xs={8} sm={2}>
+        <Fab size="small" color="secondary" aria-label="edit">
+                <EditIcon />
+          </ Fab>
+        </Grid>
+        <Grid item xs={8} sm={4}>
+          <Typography variant="h6" gutterBottom>owner: name</Typography>
+        </Grid>
+      </Grid>
+      <Box m={4} />
+      <Grid container spacing={3}>
+        <Grid item xs={8} sm={4}>
+          <Typography variant="h6" gutterBottom>creation date: date</Typography>
+        </Grid>
+        <Grid item xs={8} sm={4}>
+          <Typography variant="h6" gutterBottom>last changed: date</Typography>
+        </Grid>
+      </Grid>
+      <Box m={4} />
+      <Typography variant="h6" gutterBottom>Groupmember</Typography>
+      <Grid item xs={8} sm={4}>
+          <Card className="root" style={{minWidth: 275,     marginBottom:10, marginTop:10}}>
+            <CardActionArea >
+            <CardContent>
+                <Typography className="title" style={{fontSize: 14}} color="textPrimary">Groupname:</Typography>
+                <Typography className="title" style={{fontSize: 14}} color="textPrimary">Groupmember</Typography>
+            </CardContent>
+            </CardActionArea>    
+          </Card> 
+      </Grid>
+      <Grid item xs={8} sm={4}>
+          <Card className="root" style={{minWidth: 275,     marginBottom:10, marginTop:10}}>
+            <CardActionArea >
+            <CardContent>
+                <Typography className="title" style={{fontSize: 14}} color="textPrimary">Groupname:</Typography>
+                <Typography className="title" style={{fontSize: 14}} color="textPrimary">Groupmember</Typography>
+            </CardContent>
+            </CardActionArea>    
+          </Card> 
+      </Grid>
+      </div>
+  );
+}}
+
+export default GroupInformation;
