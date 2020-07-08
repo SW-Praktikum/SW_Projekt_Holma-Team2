fef0b208bac4a7515417e6435e7dfa9e11c024c5
KieschkelDo <dk108@hdm-stuttgart.de>
2020-07-08 15:17:34 +0200

    Methoden für Standardarticle hinzugefügt (Insert, Delete, Find_standardarticles)
    Anpassungen in ListEntry.

--- src/ListAdministration.py
+++ src/ListAdministration.py
@@ -55,8 +55,8 @@ class Administration():
         with UserGroupRelationsMapper() as mapper:
             mapper.delete_user_relations(user)
 
-        """with ListEntryMapper() as mapper:
-            mapper.delete_purchasing_user(user)"""
+        with ListEntryMapper() as mapper:
+            mapper.delete_purchasing_user(user)
 
         with GroupMapper() as mapper:
             mapper.delete_owner(user)
@@ -95,8 +95,9 @@ class Administration():
         with ShoppingListMapper() as mapper:
             return mapper.find_by_group(group_id)
 
-    """def get_standardarticles_by_group_id(self, group_id):
-        pass"""
+    def get_standardarticles_by_group_id(self, group_id):
+        with ListEntryMapper() as mapper:
+            return mapper.find_standardarticles(group_id)
 
     def add_member_to_group(self, group, user):
         with UserGroupRelationsMapper() as mapper:
@@ -109,13 +110,17 @@ class Administration():
         with UserGroupRelationsMapper() as mapper:
             mapper.remove_user_from_group(group, user)
 
-    """def add_standardarticle_to_group(self, group, list_entry):
+    def add_standardarticle_to_group(self, list_entry, group):
         with ListEntryMapper() as mapper:
-            mapper."""
+            standardarticle = mapper.set_standardarticle(list_entry)
 
-    """def remove_standardarticle_from_group(self, group, list_entry):
+            mapper.insert_standardarticle(standardarticle, group)
+
+    def delete_standardarticle(self, list_entry, group):
         with ListEntryMapper() as mapper:
-            mapper."""
+            mapper.delete_standardarticle(list_entry, group)
+
+            mapper.delete(list_entry)
 
     def create_group(self, name, user_id):
         group = Group()
