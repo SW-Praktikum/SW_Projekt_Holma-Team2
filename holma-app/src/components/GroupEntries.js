36f6f0c6b5bc782d6e46bfacacf4362e2c030067
Dennis-La <59310118+Dennis-La@users.noreply.github.com>
2020-07-10 10:40:43 +0200

    Test

--- holma-app/src/components/GroupEntries.js
+++ holma-app/src/components/GroupEntries.js
@@ -276,8 +276,6 @@ class GroupEntries extends Component{
             handleClickOpenMember={this.handleClickOpenMember}
             handleCloseMember={this.handleCloseMember}
             openMember={this.state.openMember}/>
-            <ListEntryTable shoppingListId="3000"/>
-            
           </div>
         );
     }
