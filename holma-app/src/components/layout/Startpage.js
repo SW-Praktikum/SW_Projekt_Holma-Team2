7cc4e6eca6f9ef041b84c4b28c6800448e87078c
Jonas Götz <jg133@hdm-stuttgart.de>
2020-07-20 21:01:20 +0200

    added filter and sort to startpage

--- holma-app/src/components/layout/Startpage.js
+++ holma-app/src/components/layout/Startpage.js
@@ -13,7 +13,9 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardActionArea from '@material-ui/core/CardActionArea';
-
+import Grid from '@material-ui/core/Grid';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import Radio from '@material-ui/core/Radio';
 
 class ListEntry extends Component {
     constructor(props) {
@@ -46,7 +48,7 @@ class ListEntry extends Component {
                     <TableCell padding="checkbox">
                       <Checkbox
                             color="primary"
-                            checked={this.state.checked}
+                            checked={listEntry.getChecked()}
                             onChange={this.handleChangeCheck}
                             inputProps={{ 'aria-label': 'primary checkbox' }}
                         />
@@ -68,31 +70,105 @@ class Startpage extends Component {
         this.state = {
             listEntryTableElements: [],
             userId : this.props.user.getId(),
-            userName: this.props.user.name,
+            userName: this.props.user.getName(),
             displayTable: "none",
-            displayEmptyTable: ""
+            displayEmptyTable: "",
+            retailers: [],
+            filterArticleName: "",
+            filterListName: "",
+            filterRetailerName: "",
+            filterPurchasingUserName: "",
+            filterChecked: false,
+            filterStartDate: null,
+            filterEndDate: null,
+            filterOpen: "none",
+            filterText: "Filter anzeigen",
         }
     }
 
     componentDidMount(){
         if(this.state.userId){
-            this.loadListEntries();
+            this.loadRetailers()
+            this.loadListEntries().then(() => {
+                this.filterInput();
+            })
           }
     }
 
-    getName(){
-        AppAPI.getAPI().getUserById(this.state.userId).then(usr => {
-            this.setState({
-                user: usr
-            })
+    // dropdown selector for what fo filter
+    filterInput = () => {
+        const { filterArticleName,  filterListName, filterRetailerName, filterChecked, filterStartDate, filterEndDate, listEntryTableElements} = this.state
+        
+        var filteredElements = listEntryTableElements
+
+        if (filterArticleName !== "") {
+            filteredElements =  filteredElements.filter(function(item) {
+                return item.props.listEntry.articleName.toLocaleLowerCase().includes(filterArticleName.toLocaleLowerCase());
+            });
+        }
+
+        if (filterListName !== "") {
+            filteredElements =  filteredElements.filter(function(item) {
+                return item.props.listEntry.shoppingListName.toLocaleLowerCase().includes(filterListName.toLocaleLowerCase());
+            });
+        }
+
+        if (filterRetailerName !== "") {
+            filteredElements =  filteredElements.filter(function(item) {
+                return item.props.listEntry.retailerName.toLocaleLowerCase().includes(filterRetailerName.toLocaleLowerCase());
+            });
+        }
+
+        if (filterChecked == true) {
+            filteredElements =  filteredElements.filter(function(item) {
+                return item.props.listEntry.checked == true;
+            });
+        }
+        if (filterChecked == false) {
+            filteredElements =  filteredElements.filter(function(item) {
+                return item.props.listEntry.checked == false;
+            });
+        }
+
+        this.setState({
+            filteredListEntryTableElements: filteredElements
         })
     }
 
-    loadListEntries = () => {
-        console.log("Current user id:", this.state.userId)
+    handleSort = (e, value) => {
+        // sortieren von Listeneinträgen
+        if (value === null) {
+            var filteredElements = this.state.filteredListEntryTableElements
+        }
+        else {
+            var sortInput = value.name
+            var filteredElements = this.state.filteredListEntryTableElements
+        }
+           
+        if (sortInput === "Händler") {
+            //nach Einzelhändler sortieren
+            filteredElements = filteredElements.sort((a, b) => 
+                a.props.listEntry.retailerName > b.props.listEntry.retailerName ? 1 : -1)
+        }
+        else if (sortInput === "Artikel") {
+            //nach Artikel sortieren
+            filteredElements = filteredElements.sort((a, b) => 
+                a.props.listEntry.articleName > b.props.listEntry.articleName ? 1 : -1)
+        }
+        else if (sortInput === "Liste") {
+            //nach Shoppinglist sortieren
+            filteredElements = filteredElements.sort((a, b) => 
+                a.props.listEntry.shoppingListName > b.props.listEntry.shoppingListName ? 1 : -1)
+        }
+        this.setState({
+            filteredListEntryTableElements: filteredElements
+        })
+    }
 
+
+    loadListEntries = () => {
         // get listentries by user ID
-        AppAPI.getAPI().getListEntriesByUserId(this.state.userId).then(listEntries => {
+        return AppAPI.getAPI().getListEntriesByUserId(this.state.userId).then(listEntries => {
             if (listEntries.length !== 0) {
                 this.setState({
                     displayTable: "",
@@ -106,13 +182,14 @@ class Startpage extends Component {
                 })
             }
             console.log("Loaded list entries for user '" + this.state.userId + "':", listEntries)
-            var listEntryTableElements = listEntries.map((listEntry) => <ListEntry userId = {this.state.userId} listEntry={listEntry} loadListEntries={this.loadListEntries} />)
-
+            var listEntryTableElements = listEntries.map((listEntry) => <ListEntry listEntry={listEntry} loadListEntries={this.loadListEntries} />)
             this.setState({
                 listEntryTableElements: listEntryTableElements,
+                filteredListEntryTableElements: listEntryTableElements,
                 loadingInProgress: true, // loading indicator 
                 loadingError: null
                 })
+            return new Promise(function (resolve) {resolve(listEntries)})
             }).catch(e =>
                 this.setState({ // Reset state with error from catch 
                 loadingInProgress: false,
@@ -121,28 +198,205 @@ class Startpage extends Component {
         );  
     }
 
+    loadRetailers = () => {
+        AppAPI.getAPI().getRetailers().then((retailers) => {
+            console.log("Loaded all retailers:", retailers)
+            this.setState({
+            retailers: retailers,
+            loadingInProgress: true, // loading indicator 
+            loadingError: null,
+        });
+        }).catch(e =>
+            this.setState({ // Reset state with error from catch 
+                loadingInProgress: false,
+                loadingError: e
+            })
+            );  
+    } 
+
+    handleInputChangeTextField = async (e) => {
+        let key = e.target.id
+        let val = e.target.value
+
+        await this.setState({[key] : val})
+        await this.filterInput()
+    }
+
+    handleCheckButton = async (check) => {
+        await this.setState({filterChecked : check})
+        await this.filterInput()
+    }
+
+    handleInputChangeAutoComplete = async (key, obj) => {
+        let val = ""
+        if (obj !== null) {
+            val = obj.getName()
+        }
+        
+        await this.setState({[key] : val})
+        this.filterInput()
+    }
+    
+    handleFilterOpen = () => {
+        if (this.state.filterOpen === "none") {
+            this.setState({
+                filterOpen: "",
+                filterText: "Filter ausblenden"
+            })
+        }
+
+        else {
+            this.setState({
+                filterOpen: "none",
+                filterText: "Filter anzeigen"
+            })
+        }
+    }
+
     render() {
-        const {retailers} = this.state;
-        console.log("ListElements:", this.state.listEntryTableElements)
+        const sortFunctions = [
+            {
+                name: "Artikel",
+            },
+            {
+                name: "Händler",
+            },
+            {
+                name: "Liste",
+            },
+        ]
+        const {retailers, filterArticleName, filterListName, filterRetailerName, filterChecked, filterStartDate, filterEndDate, filteredListEntryTableElements, userName} = this.state;
         return (
             <React.Fragment>             
-                <Card style={{display: this.state.displayTable, minWidth: '100%', marginBottom:15, marginTop:15, }}>
-                    <CardContent>
-                    <Typography align="left" className="title" style={{fontSize: 16, fontWeight: "bold", color: colors.teal[600]}}>
-                        Hallo {this.state.userName},
-                    </Typography>
+                <Grid 
+                    container 
+                    direction="row" 
+                    justify="space-between" 
+                    alignItems="center" 
+                    component={Paper} 
+                    style={{display: this.state.displayTable, minWidth: '100%', marginBottom:15, marginTop:15, }}
+                    >
+                    <Grid item xs={12} sm={4} style={{paddingLeft: 20, paddingRight: 20, paddingTop: 10, paddingBottom: 10}}>
+                        <Typography align="left" className="title" style={{fontSize: 16, fontWeight: "bold", color: colors.teal[600]}}>
+                            Hallo {userName},
+                        </Typography>
                         <Typography align="left" className="title" style={{fontSize: 16, fontWeight: "bold", color: colors.teal[600]}}>Deine persönliche Einkaufsliste:</Typography>
-                    </CardContent> 
-                </Card>
-                <Card style={{display: this.state.displayEmptyTable, minWidth: '100%', marginBottom:15, marginTop:15, }}>
-                    <CardContent>
-                    <Typography align="left" className="title" style={{fontSize: 16, fontWeight: "bold", color: colors.teal[600]}}>
-                        Hallo {this.state.userName},
-                    </Typography>
-                        <Typography align="left" className="title" style={{fontSize: 16, fontWeight: "bold", color: colors.teal[600]}}>Du hast noch keine Listeneinträge die dir zugeordnet sind.</Typography>
-                    </CardContent>
-                </Card>
+                    </Grid>
+                    <Grid item xs={12} sm={4} style={{paddingLeft: 20, paddingRight: 20, paddingTop: 10, paddingBottom: 10}}>
+                        <Button 
+                            variant="contained"
+                            fullWidth 
+                            color="primary"
+                            onClick={this.handleFilterOpen}>
+                            {this.state.filterText}
+                        </Button>
+                    </Grid>
+                    
+                    
+                    <Grid item xs={12} sm={4} style={{paddingLeft: 20, paddingRight: 20, paddingTop: 10, paddingBottom: 10}}>
+                        <Autocomplete
+                            id="sortSelector"
+                            onChange={(event, value) => this.handleSort("sortSelector", value)}
+                            options={sortFunctions}
+                            
+                            defaultValue="filtern"
+                            getOptionLabel={(option) => option.name}
+                            renderInput={(params) => (
+                                <TextField {...params} variant="outlined" label="Sortieren" placeholder="Sortieren" />
+                            )}                
+                        />
+                    </Grid>
+                </Grid>
 
+                <Grid 
+                    container 
+                    direction="row" 
+                    justify="space-between" 
+                    alignItems="center" 
+                    component={Paper} 
+                    style={{display: this.state.displayEmptyTable, minWidth: '100%', marginBottom:15, marginTop:15, }}
+                    >
+                    <Grid item xs={12} sm={4} style={{paddingLeft: 20, paddingRight: 20, paddingTop: 10, paddingBottom: 10}}>
+                        <Typography align="left" className="title" style={{fontSize: 16, fontWeight: "bold", color: colors.teal[600]}}>
+                            Hallo {userName},
+                        </Typography>
+                        <Typography align="left" className="title" style={{fontSize: 16, fontWeight: "bold", color: colors.teal[600]}}>Du hast noch keine Listeneinträge die dir zugeordnet sind.</Typography>
+                    </Grid>
+                    <Grid item xs={12} sm={4}/>
+                    <Grid item xs={12} sm={4}/>
+                </Grid>
+                
+                <Grid container direction="row" justify="space-between" alignItems="center" component={Paper} style={{marginTop: 15, display: this.state.filterOpen}}>
+                    <Grid item xs={12} sm={4} style={{paddingLeft: 20, paddingRight: 20, paddingTop: 10, paddingBottom: 10}}>
+                        <Typography color="primary" style={{fontSize: 18}}>
+                            Filter nach:
+                        </Typography> 
+                    </Grid>    
+                    <Grid item xs={12} sm={4} style={{paddingLeft: 20, paddingRight: 20, paddingTop: 10, paddingBottom: 10}}>
+                        <TextField
+                                autoFocus
+                                onChange={this.handleInputChangeTextField}
+                                margin="dense"
+                                id="filterArticleName"
+                                label="Artikel"
+                                type="ID"
+                                fullWidth
+                                value={filterArticleName}
+                            /> 
+                    </Grid>
+                    <Grid item xs={12} sm={4} style={{paddingLeft: 20, paddingRight: 20, paddingTop: 10, paddingBottom: 10}}>
+                        <Autocomplete
+                            id="filterRetailerName"
+                            onChange={(event, value) => this.handleInputChangeAutoComplete("filterRetailerName", value)}
+                            options={retailers} //liste der retailer laden
+                            defaultValue={filterRetailerName}
+                            getOptionLabel={(option) => option.name}
+                            renderInput={(params) => (
+                                <TextField {...params} variant="standard" label="Händler" placeholder="Händler" />
+                            )}                
+                        />
+                    </Grid>
+                    <Grid item xs={12} sm={4} style={{paddingLeft: 20, paddingRight: 20, paddingTop: 10, paddingBottom: 10}}>
+                        <Radio
+                            checked={filterChecked === true}
+                            onChange={() => this.handleCheckButton(true)}
+                            //value="true"
+                            color="primary"
+                            name="radio-button"
+                            //inputProps={{ 'aria-label': 'TRUE' }}
+                        /> abgehakt
+                        <Radio
+                            checked={filterChecked === false}
+                            onChange={() => this.handleCheckButton(false)}
+                            //value="d"
+                            color="primary"
+                            name="radio-button"
+                            //inputProps={{ 'aria-label': 'D' }}
+                        /> offen
+                        <Radio
+                            checked={filterChecked === "all"}
+                            onChange={() => this.handleCheckButton("all")}
+                            //value="e"
+                            color="primary"
+                            name="radio-button"
+                            //inputProps={{ 'aria-label': 'E' }}
+                        /> alle
+                    </Grid>
+                    <Grid item xs={12} sm={4} style={{paddingLeft: 20, paddingRight: 20, paddingTop: 10, paddingBottom: 10}}>
+                        <TextField
+                            autoFocus
+                            onChange={this.handleInputChangeTextField}
+                            margin="dense"
+                            id="filterListName"
+                            label="Liste"
+                            type="ID"
+                            fullWidth
+                            value={filterListName}
+                        /> 
+                    </Grid>  
+                    <Grid item xs={12} sm={4}/>
+                </Grid>
+                
                 <TableContainer component={Paper} style={{display: this.state.displayTable}}>
                     <Table aria-label="collapsible table">                   
                         <TableHead style={{backgroundColor: colors.teal[600]}}>
@@ -166,7 +420,7 @@ class Startpage extends Component {
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {this.state.listEntryTableElements}
+                            {filteredListEntryTableElements}
                         </TableBody>
                     </Table>
                 </TableContainer>
